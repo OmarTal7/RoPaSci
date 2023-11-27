cc48505ee373ce8c6ef46d69eacaff8fca216f07
Omar <85651020+OmarTal7@users.noreply.github.com>
2023-11-27 18:00:12 -0500

    Created file structure and first draft of pseudocode

---
+++ game-logic.js
@@ -0,0 +1,14 @@
+// Check game counter <5 -- if not, GAME OVER 
+// set up list of options: rock, paper, or scissors
+// let comp pick one
+// prompt user to pick on
+// check user input:
+//      if it doesn't match any of the 3 options, return error
+//      if it matches, go on to game logic
+//          if choices match, its a draw.
+//          if rock/paper: user wins, +1 counter
+//          if paper/rock: comp wins, +1 counter
+//          if scissor/paper: comp wins, +1 counter
+//          if paper/scissor: user wins, +1 counter
+//          if rock/scissor: comp wins, +1 counter
+//          if scissor/rock: user wins, +1 counter
