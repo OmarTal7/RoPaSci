165c93dad3b9e107132f7a389db19adf364a8aa9
Omar <85651020+OmarTal7@users.noreply.github.com>
2023-11-28 13:56:17 -0500

    bunch of changes: added game function/logic, added singleRound function/logic. Still missing:game counter/score with a while loop implementation.

--- game-logic.js
+++ game-logic.js
@@ -13,25 +13,60 @@
 //          if rock/scissor: comp wins, +1 counter
 //          if scissor/rock: user wins, +1 counter
 
-let gameCounter = 0;
-const gameChoices = ["rock", "paper", "scissors"];
-
 // Gets random choice from gameChoices array
-function getComputerChoice(){
-    return (gameChoices[Math.floor(Math.random() * 4)])
-}
+// function getComputerChoice(){
+//     return (gameChoices[Math.floor(Math.random() * 3)])
+// }
 
-function singleRound (computerSelection, playerSelection){
-    let player = prompt("Rock..Paper..Scissors.. SHOOT!");
+function singleRound(computerSelection, playerSelection){
+
+    if (computerSelection == playerSelection){
+            return `It's a draw! You both picked ${computerSelection}!`;
+    } 
+    else if ((computerSelection == "rock" && playerSelection == "paper") ||
+            (computerSelection == "paper" && playerSelection == "scissor") ||
+            (computerSelection == "scissor" && playerSelection == "rock")){
+                
+                // ++gameCounter;
+                return `You win! comp: ${computerSelection}, player: ${playerSelection}`;
+
+            }
     
-    while(!gameChoices.includes(player.toLocaleLowerCase())){
+    else if ((playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissor") ||
+    (playerSelection == "scissor" && computerSelection == "rock")){
         
-    }    
-    
-    
-    //Don't forget to increment counter
+        // ++gameCounter;
+        return `You lose! comp: ${computerSelection}, player: ${playerSelection}`;
+
+
+    }
+
+
 }
 
+       
 
 
-(gameCounter > 5) ? console.log("GAME OVER") : singleRound()
+function game(){
+    let gameCounter = 0;
+    const gameChoices = ["rock", "paper", "scissors"];
+
+    // while(gameCounter < 5){
+        let player = prompt("Rock, paper, scissors,...SHOOT!");
+        
+        // Player doesn't choose one of the options
+        while(!gameChoices.includes(player.toLowerCase())){
+            player = prompt("Please choose either Rock, Paper, or Scissors!");
+            if (gameChoices.includes(player.toLowerCase())){break;}
+        }
+
+        return console.log(singleRound(gameChoices[Math.floor(Math.random() * 3)], player));
+    // }
+    
+}
+
+// singleRound();
+game();
+
+// (gameCounter > 5) ? console.log("GAME OVER") : singleRound()
