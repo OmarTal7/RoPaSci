b1435c3b4fdbf7670efbe3f30892bf047a28221c
Omar <85651020+OmarTal7@users.noreply.github.com>
2023-11-29 11:53:06 -0500

    Cleaned up loose comments and spacing

--- game-logic.js
+++ game-logic.js
@@ -1,23 +1,3 @@
-// Check game counter <5 -- if not, GAME OVER 
-// set up list of options: rock, paper, or scissors
-// let comp pick one
-// prompt user to pick on
-// check user input:
-//      if it doesn't match any of the 3 options, return error
-//      if it matches, go on to game logic
-//          if choices match, its a draw.
-//          if rock/paper: user wins, +1 counter
-//          if paper/rock: comp wins, +1 counter
-//          if scissor/paper: comp wins, +1 counter
-//          if paper/scissor: user wins, +1 counter
-//          if rock/scissor: comp wins, +1 counter
-//          if scissor/rock: user wins, +1 counter
-
-// Gets random choice from gameChoices array
-// function getComputerChoice(){
-//     return (gameChoices[Math.floor(Math.random() * 3)])
-// }
-
 // Given two selections, function determines winner of a RPC round
 function singleRound(computerSelection, playerSelection){
 
